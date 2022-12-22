18e779b7a73b4145479fe231786b7cea6291a79f
Laxman Singh Tomar <laxman.singh@emplay.net>
2022-12-22 15:26:40 +0530

    Updated Model

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import recall_score, precision_score
 import json
 import os
-import numpy as np
-import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Read in data
 X_train = np.genfromtxt("data/train_features.csv")
@@ -12,25 +12,21 @@ y_train = np.genfromtxt("data/train_labels.csv")
 X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
-
 # Fit a model
+depth = 5
+clf = RandomForestClassifier(max_depth=depth)
+clf.fit(X_train, y_train)
 
-clf = MLPClassifier(random_state=0, max_iter=3)
-clf.fit(X_train,y_train)
-
-# Get overall accuracy
 acc = clf.score(X_test, y_test)
+print(acc)
 
-# Get precision and recall
-y_score = clf.predict(X_test)
-prec = precision_score(y_test, y_score)
-rec = recall_score(y_test,y_score)
+metrics = """
+Accuracy: {:10.4f}
+![Confusion Matrix](plot.png)
+""".format(acc)
+with open("metrics.txt", "w") as outfile:
+    outfile.write(metrics)
 
-# Get the loss
-loss = clf.loss_curve_
-pd.DataFrame(loss, columns=["loss"]).to_csv("loss.csv", index=False)
-
-with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc, "precision":prec,"recall":rec}, outfile)
-
-
+# Plot it
+disp = plot_confusion_matrix(clf, X_test, y_test, normalize="true", cmap=plt.cm.Blues)
+plt.savefig("plot.png")
